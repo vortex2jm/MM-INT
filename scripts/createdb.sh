8fe10f6c68c592ef951ebc372594225748e8e7b0
vortex2jm <joaopaulomoura5328@gmail.com>
2024-03-08 03:13:15 +0000

    Changing volume path

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -1,5 +1,5 @@
 docker run \
     --name influx_db -d \
     -p 8086:8086 \
-    -v /home/joao/Code/ic/MPolka-Int-Database/influxdb_volume:/var/lib/influxdb2 \
+    -v /home/vagrant/MM-INT/influxdb_volume:/var/lib/influxdb2 \
     influxdb:latest
